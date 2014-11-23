69215da42cf1f05b079684d0a8127f2414f96ae3
Sheraff <fpellet@ensc.fr>
2014-11-23 21:06:32 +0100

    update to gh-pages branch's state

--- s.js
+++ s.js
@@ -1,4 +1,24 @@
 var logo = document.getElementById('logo');
-logo.onclick = function (event) {
-	document.body.className = document.body.className === "launcher" ? "" : "launcher"
-}
+var input = document.getElementById('input');
+
+
+var propagate_on_touch = document.querySelectorAll('[data-propagation-target]');
+for (var i = propagate_on_touch.length - 1; i >= 0; i--) {
+    propagate_on_touch[i].propagation_target = (function(origin_element) {
+        this.setAttribute('data-launcher', this.getAttribute('data-launcher') === 'true' ? 'false' : 'true');
+    }).bind(propagate_on_touch[i]);
+};
+logo.addEventListener('click', function(event) {
+    input.focus();
+    this.propagation_target(this);
+    var targets = document.querySelectorAll('[data-propagation-target="' + this.getAttribute('data-propagation-target') + '"]');
+    for (var j = targets.length - 1; j >= 0; j--) {
+        if (targets[j] != this) {
+            if (targets[j].propagation_timeout) clearTimeout(targets[j].propagation_timeout);
+            var targetBoundingRect = targets[j].getBoundingClientRect();
+            var delay = Math.sqrt(Math.pow(event.pageX - (event.pageX > targetBoundingRect.right ? targetBoundingRect.right : (event.pageX < targetBoundingRect.left ? targetBoundingRect.left : event.pageX)), 2) + Math.pow(event.pageY - (event.pageY > targetBoundingRect.bottom ? targetBoundingRect.bottom : (event.pageY < targetBoundingRect.top ? targetBoundingRect.top : event.pageY)), 2))
+            targets[j].propagation_timeout = window.setTimeout(targets[j].propagation_target.bind(undefined, this), 1000 * delay / window.innerWidth);
+        }
+    };
+    event.stopPropagation();
+});
